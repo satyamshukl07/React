e807eaecd1308d08d71b611e4e7bbb0a71a73f45
satyam shukla <satyamshukla9528@gmail.com>
2026-03-28 16:12:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
+
+
+// function Description(){
+//   return <h3> Iam The  Description</h3>;
+// }
+
+
+function App() {
+
+  return <ProductTab/>; 
+  //   <Title/>
+  //   <Title/>
+  //   <Title/>
+  // </>;
+}
+
+export default App
---
+++ src/Product.jsx
@@ -0,0 +1,13 @@
+import "./Product.css";
+function Product ({title, price, features}){ 
+         return(
+        <div className =" Product">
+            <h3> {title}</h3>
+            <h5> Price :{price}</h5>
+            {price>30000? <p>"Discount of 5%"</p> : null }
+                    </div>
+    );
+
+    }
+
+export default Product;
